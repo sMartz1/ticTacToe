4962284593e16bd90c682424e7903c885476ea07
Sebastian M <ra3gan1991@gmail.com>
2021-11-03 10:30:48 +0100

    Se añade funcionalidad de click y boton de reset para test

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-//Con el fin de llevar seguimiento de cada casilla, creamos atributo position en html
+//TICTACTOE
 
+// 0 = player1   1 = player2
+let currentPlayer = 1;
+let isPlaying = false;
 // 0 = empty  1 = X   2 = 0
-let currentState = [0, 1, 0, 2, 0, 0, 1, 0, 0];
-// 0 =
+let currentState = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+//Function to draw all the items
 function draw() {
+    //We remove all childs from gameScreen
     let container = document.getElementById("screen");
     while (container.firstChild) {
         container.firstChild.remove()
     }
+
     let rowI = 0;
     let rowC = 0;
+    //We create the 3 rows with 3 elements each one and draw the content depending on currentState
     while (rowI < 3) {
         let tempRow = document.createElement("div");
         tempRow.classList.add("row");
         for (let i = 0; i < currentState.length; i++) {
-            console.log("yep")
+
             let item = document.createElement("div");
             item.classList.add("item");
+            item.setAttribute("position", i);
+            item.addEventListener("click", e => {
+                currentState[parseInt(e.target.getAttribute("position"))] = currentPlayer;
+                draw();
+            });
             item.innerHTML = currentState[i] == 0 ? "" : currentState[i] == 1 ? "X" : "O";
             tempRow.appendChild(item);
             rowC++;
@@ -29,7 +41,14 @@ function draw() {
             }
         }
     }
-
-
 }
+
+function resetClick() {
+    currentState = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    draw();
+}
+
+
+
+
 draw();
